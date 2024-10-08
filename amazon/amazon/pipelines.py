14201057eecdfe7fd8a2abe2d8f9327bae674d7c
VanVu <trinhvanvu@gmail.com>
2024-10-08 21:44:21 +0700

    pipeline csv

--- amazon/amazon/pipelines.py
+++ amazon/amazon/pipelines.py
@@ -6,8 +6,34 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import scrapy
+import pymongo 
+from pymongo import MongoClient
+import json
+from scrapy.exceptions import DropItem
+import csv
+import os
 
-
-class AmazonPipeline:
+class CSVDBAmazonPipeline:
     def process_item(self, item, spider):
+        with open('amazon.csv', 'a', encoding='utf-8', newline='') as file:
+            writer = csv.writer(file, delimiter='$')
+            writer.writerow([
+                item['productid'], 
+                item['title'],
+                item['author'],
+                item['publication_date'],
+                item['old_price'], 
+                item['new_price'],
+                item['rating'],
+                item['reviews'], 
+                item['page_count'],
+                item['language'],
+                item['publisher_name'], 
+                item['review_name'],
+                item['review_rating'],
+                item['review_comment'], 
+                item['review_date']
+            ])
         return item
+    pass
