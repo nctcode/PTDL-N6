ccf129674494d65e49b42496d01d3dabbdddd19c
BoMagic123 <manhkaka195@gmail.com>
2024-10-10 14:18:59 +0700

    update setting.py

--- amazon/amazon/settings.py
+++ amazon/amazon/settings.py
@@ -1,93 +1,60 @@
-# Scrapy settings for amazon project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-
 BOT_NAME = "amazon"
 
 SPIDER_MODULES = ["amazon.spiders"]
 NEWSPIDER_MODULE = "amazon.spiders"
 
+ROBOTSTXT_OBEY = False
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "amazon (+http://www.yourdomain.com)"
+# Tăng số lượng yêu cầu đồng thời
+CONCURRENT_REQUESTS = 50
+CONCURRENT_REQUESTS_PER_DOMAIN = 50
+CONCURRENT_REQUESTS_PER_IP = 16
 
-# Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# Giảm độ trễ giữa các yêu cầu
+DOWNLOAD_DELAY = 1
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
+# Bật AutoThrottle để tự động điều chỉnh tốc độ
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 1  # Giảm độ trễ bắt đầu
+AUTOTHROTTLE_MAX_DELAY = 3  # Tăng cường độ tối đa
+AUTOTHROTTLE_TARGET_CONCURRENCY = 5.0  # Tăng tốc độ yêu cầu
 
-# Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+# Điều chỉnh header để trông giống yêu cầu từ trình duyệt thật
+DEFAULT_REQUEST_HEADERS = {
+    'Accept-Language': 'en-US,en;q=0.9',
+    'DNT': '1',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Referer': 'https://www.amazon.com/',
+}
 
-# Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# Tăng số lần retry nhưng ở mức hợp lý để không làm chậm quá trình cào dữ liệu
+RETRY_TIMES = 10
+RETRY_DELAY = 3  # Điều chỉnh thời gian delay giữa các lần retry
 
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# Cài đặt thời gian chờ tải xuống hợp lý để tránh việc tải trang quá lâu
+DOWNLOAD_TIMEOUT = 10
 
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#    "Accept-Language": "en",
-#}
+# Cấu hình middlewares để sử dụng Random User-Agent
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+}
 
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    "amazon.middlewares.AmazonSpiderMiddleware": 543,
-#}
+# Cấu hình pipelines để lưu dữ liệu vào MongoDB, JSON và CSV
+ITEM_PIPELINES = {
+    "amazon.pipelines.CSVDBAmazonPipeline": 100,
+    "amazon.pipelines.JsonDBAmazonPipeline": 200,
+    "amazon.pipelines.MongoDBAmazonPipeline": 400
+}
 
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "amazon.middlewares.AmazonDownloaderMiddleware": 543,
-#}
+# Vô hiệu hóa HTTP cache
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 86400
 
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    "scrapy.extensions.telnet.TelnetConsole": None,
-#}
-
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "amazon.pipelines.AmazonPipeline": 300,
-#}
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = "httpcache"
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
-
-# Set settings whose default value is deprecated to a future-proof value
+# Cấu hình phiên bản của request fingerprinter và reactor
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+# Đặt giới hạn số lượng item cào được trước khi đóng spider
+CLOSESPIDER_ITEMCOUNT = 1800
